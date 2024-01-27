67e84de25600d26263232a7b18c4162b1a89d317
Adam <97370646+anon-adam@users.noreply.github.com>
2024-01-26 23:59:40 -0500

    Shorten countdown command

--- cogs/countdown.py
+++ cogs/countdown.py
@@ -1,14 +1,15 @@
 from discord.ext import commands
 import asyncio
 
+
 class Countdown(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        
-    @commands.command(name='countdown')
+
+    @commands.command(name="cd")
     @commands.cooldown(1, 10, commands.BucketType.guild)
     async def count(self, ctx):
-        msg = await ctx.send('10')
+        msg = await ctx.send("10")
         await asyncio.sleep(1)
         for num in range(9, 0, -1):
             try:
@@ -21,7 +22,10 @@ class Countdown(commands.Cog):
     @count.error
     async def count_error(self, ctx, error):
         if isinstance(error, commands.CommandOnCooldown):
-            await ctx.send("This command can only be used once every 10 seconds per server.")
+            await ctx.send(
+                "This command can only be used once every 10 seconds per server."
+            )
+
 
 async def setup(bot):
     await bot.add_cog(Countdown(bot))

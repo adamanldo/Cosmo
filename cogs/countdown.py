84dc5224e50c2097faa5be443bb84b634010cef6
Adam <97370646+anon-adam@users.noreply.github.com>
2024-08-18 00:36:49 -0400

    Add short countdown

--- cogs/countdown.py
+++ cogs/countdown.py
@@ -19,11 +19,31 @@ class Countdown(commands.Cog):
                 return
         await msg.edit(content="Go!")
 
+    @commands.command(name="scd")
+    @commands.cooldown(1, 5, commands.BucketType.guild)
+    async def short_count(self, ctx):
+        msg = await ctx.send("5")
+        await asyncio.sleep(1)
+        for num in range(4, 0, -1):
+            try:
+                await msg.edit(content=num)
+                await asyncio.sleep(1)
+            except:
+                return
+        await msg.edit(content="Go!")
+
     @count.error
     async def count_error(self, ctx, error):
         if isinstance(error, commands.CommandOnCooldown):
             await ctx.send(
                 "This command can only be used once every 10 seconds per server."
+            )
+
+    @short_count.error
+    async def short_count_error(self, ctx, error):
+        if isinstance(error, commands.CommandOnCooldown):
+            await ctx.send(
+                "This command can only be used once every 5 seconds per server."
             )
 
 
